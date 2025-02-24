204bfadd43891f8452e675e8232632dd41954243
Diwakar Kumar <diwakarkumar9451@gmail.com>
2025-02-24 20:34:33 +0530

    Add files via upload

---
+++ dom1.js
@@ -0,0 +1,9 @@
+
+let divElement = document.getElementById("myDiv");
+divElement.style.backgroundColor = "red"; 
+divElement.style.margin = "50px"; 
+divElement.style.padding = "50px"; 
+divElement.style.fontSize = "30px"; 
+divElement.style.fontWeight = "bold"; 
+divElement.style.height = "300px"; 
+divElement.style.width = "500px";
